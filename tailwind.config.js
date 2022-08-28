3693689b99b35ee50bd4e5f6cab1b6b8d5129889
magnopachecoluiz <magno.luiz@nicephotos.net>
2022-08-28 14:33:07 -0300

    wip solving

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,19 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
 module.exports = {
-  prefix: '',
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './src/**/*.{html, scss, ts}',
-    ]
-  },
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      Nunito: ['Nunito', 'sans-serif'],
-      Norms: ['TT Norms Pro', 'sans-serif'],
-      Roboto: ['Roboto', 'sans-serif'],
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './src/**/*.{html, ts}',
+      ]
     },
-    boxShadow: {
-      'movieCard': '0px 2px 6px rgba(0, 0, 0, 0.16)',
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
     },
-    screens: {
-      'threeColumns': '517px',
-      'fourColumns': '689px',
-      'fiveColumns': '912px',
-      'sixColumns': '1094px',
-      'sevenColumns': '1250px',
+    variants: {
+      extend: {},
     },
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
 };
