374255e540d18fc308d687ccdfd690a58e62dac4
magnopachecoluiz <magno.luiz@nicephotos.net>
2022-08-28 14:38:06 -0300

    wip solving

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,24 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
+      fontFamily: {
+        Nunito: ['Nunito', 'sans-serif'],
+        Norms: ['TT Norms Pro', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif'],
+      },
+      boxShadow: {
+        'movieCard': '0px 2px 6px rgba(0, 0, 0, 0.16)',
+      },
+      screens: {
+        'threeColumns': '517px',
+        'fourColumns': '689px',
+        'fiveColumns': '912px',
+        'sixColumns': '1094px',
+        'sevenColumns': '1250px',
+      },
       extend: {},
     },
+
     variants: {
       extend: {},
     },
